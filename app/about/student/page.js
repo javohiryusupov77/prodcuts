89dfdac55ec6c4f9de6e7b4a03fa31431d98a5f0
Yusupov Javohir <javohiryyusupov6@gmail.com>
2024-07-28 16:26:26 +0500

    commit last

--- app/about/student/page.js
+++ app/about/student/page.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function page() {
   return (
     <div>
-      <h1 style={{textAlign:"center"}}>Still creating</h1>
+      <h1 style={{ textAlign: "center", fontSize: "50px" }}>
+        still being created
+      </h1>
     </div>
   );
 }
